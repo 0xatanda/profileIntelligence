d9e9f33d195f0a544bdcc7f25f813fe5240b4ee3
0xatanda <atanda0x@gmail.com>
2026-04-18 17:10:08 +0100

    fixed: broken db connect

--- internal/db/db.go
+++ internal/db/db.go
@@ -11,20 +11,14 @@ import (
 func Connect() *sql.DB {
 	dsn := os.Getenv("DATABASE_URL")
 
-	if dsn == "" {
-		log.Fatal("DATABASE_URL is not set")
-	}
-
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
-		log.Fatal("failed to open DB:", err)
+		log.Fatal(err)
 	}
 
 	if err := db.Ping(); err != nil {
-		log.Fatal("failed to connect DB:", err)
+		log.Fatal("DB connection failed:", err)
 	}
-
-	log.Println("✅ Connected to database")
 
 	return db
 }
